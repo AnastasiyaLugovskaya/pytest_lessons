5aa9c49703875169796c3906b3b6faad06f7886e
Anastasiya Lugovskaya <Nastacya.l@yandex.ru>
2024-10-13 15:59:06 +0300

    Add exercises from 2nd lesson

---
+++ code/tasks_proj/tests/func/test_unique_id_1.py
@@ -0,0 +1,24 @@
+import pytest
+
+import tasks
+from tasks import Task
+
+
+@pytest.fixture(autouse=True)
+def initialized_tasks_db(tmpdir):
+    """Connect to db before testing, disconnect after."""
+    # Setup : start db
+    tasks.start_tasks_db(str(tmpdir), 'tiny')
+
+    yield  # здесь происходит тестирование
+
+    # Teardown : stop db
+    tasks.stop_tasks_db()
+
+
+@pytest.mark.skip(reason='misunderstood the API')
+def test_unique_id():
+    """Вызов unique_id () дважды должен возвращать разные числа."""
+    id_1 = tasks.unique_id()
+    id_2 = tasks.unique_id()
+    assert id_1 != id_2
